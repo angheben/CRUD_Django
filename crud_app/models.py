79893bf48bb8e0b3ff17dae5aa3272c77088133c
Vitor Angheben <vitor_angheben@hotmail.com>
2022-11-11 20:50:14 -0300

    __str__ method added to models.py, check_data.html ok models.py almost finished

--- crud_app/models.py
+++ crud_app/models.py
@@ -5,9 +5,11 @@ class User(models.Model):
     """
     A normal class that represents a User object, the attributes are those bellow:
     """
-    objects = None
     first_name = models.CharField(name="First Name", max_length=30)
     last_name = models.CharField(name="Last Name", max_length=30)
     cpf = models.CharField(name="CPF", max_length=30)
     age = models.IntegerField(name="Age")
     email = models.EmailField(name="email", max_length=30)
+
+    def __str__(self):
+        return f"{self.first_name}"
